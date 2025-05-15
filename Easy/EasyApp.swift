142454a040fce246317d242c8f1e3d59e9aab879
Elai Zuberman <elai950@gmail.com>
2025-05-15 14:33:56 +0300

    Initial Commit

---
+++ Easy/EasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyApp.swift
+//  Easy
+//
+//  Created by Elai Zuberman on 15/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct EasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
